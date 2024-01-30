918fc6ae01b9c0c0d9e847eff92eed42de8a9f82
foticc <foticc@163.com>
2024-01-31 01:37:56 +0800

    /

---
+++ api-config-server/src/main/java/com/foitcc/apiconfigserver/IndexController.java
@@ -0,0 +1,21 @@
+package com.foitcc.apiconfigserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class IndexController {
+
+
+    @Value("${data.time}")
+    private String data;
+
+
+    @GetMapping("/data")
+    public String data() {
+        return data;
+    }
+}
