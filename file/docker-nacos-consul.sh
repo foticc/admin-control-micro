199d28621c796187a94c1eb321aa42d397c7a4b9
foticc <foticc@163.com>
2024-03-15 22:45:35 +0800

    /

--- file/docker-nacos-consul.sh
+++ file/docker-nacos-consul.sh
@@ -1,4 +1,4 @@
-docker run --restart=always -d -p 8500:8500 -p 8600:8600/udp --name=badger hashicorp/consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
+docker run --restart=always --name=consul -d -p 8500:8500 -p 8600:8600/udp --name=badger hashicorp/consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
 
 
-docker run --restart=always --env PREFER_HOST_MODE=hostname --env MODE=standalone --env NACOS_AUTH_ENABLE=true -p 8848:8848 -p 9848:9848 -p 9849:9849 nacos/nacos-server:v2.1.1
+docker run --restart=always --name=nacos --env PREFER_HOST_MODE=hostname --env MODE=standalone --env NACOS_AUTH_ENABLE=true -p 8848:8848 -p 9848:9848 -p 9849:9849 nacos/nacos-server:v2.1.1
