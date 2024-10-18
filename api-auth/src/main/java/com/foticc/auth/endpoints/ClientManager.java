49aa3b9b76d91f95820198aac2791e98c79366fd
foticc <foticc@163.com>
2024-10-18 18:31:17 +0800

    remove nacos

--- api-auth/src/main/java/com/foticc/auth/endpoints/ClientManager.java
+++ api-auth/src/main/java/com/foticc/auth/endpoints/ClientManager.java
@@ -2,13 +2,17 @@ package com.foticc.auth.endpoints;
 
 import com.foticc.auth.endpoints.service.ClientService;
 import com.foticc.auth.oauth2.domain.Client;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/client/manager")
 @RestController
 public class ClientManager {
@@ -22,11 +26,11 @@ public class ClientManager {
 
 
     @GetMapping("/page")
-    public Page<Client> page(@RequestParam(value = "name",required = false) String name,
-                                @RequestParam("page") Integer page,
-                                @RequestParam("size") Integer size
+    public Page<Client> page(@PageableDefault Pageable page,
+                             @RequestParam(value = "name",required = false) String name
                              ) {
-        return clientService.clientPage(PageRequest.of(page,size),name);
+        log.info("{}",page);
+        return clientService.clientPage(page,name);
     }
 
 
